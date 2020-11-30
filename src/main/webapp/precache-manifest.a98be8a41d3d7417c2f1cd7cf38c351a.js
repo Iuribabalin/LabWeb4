298a31764c8f7185a03ef056ac5e4e8171b428c6
Iuribabalin <babalin2011@gmail.com>
2020-12-01 00:23:55 +0300

    Initial commit

---
+++ src/main/webapp/precache-manifest.a98be8a41d3d7417c2f1cd7cf38c351a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f3c39456348ad254d7",
+    "url": "/css/app.07e3ee2b.css"
+  },
+  {
+    "revision": "a0858fc038701fb657a3b9953c61dae8",
+    "url": "/img/duck.a0858fc0.png"
+  },
+  {
+    "revision": "b77a8cd6396137146b4dd6957dd6ee57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7f3c39456348ad254d7",
+    "url": "/js/app.f360f141.js"
+  },
+  {
+    "revision": "de3d31214ea52aec6711",
+    "url": "/js/chunk-44844718.0439b909.js"
+  },
+  {
+    "revision": "d289a55cddd21c1d2141",
+    "url": "/js/chunk-5f4ad635.d790afa6.js"
+  },
+  {
+    "revision": "5b15dd16911c434ecac6",
+    "url": "/js/chunk-61d7212a.210d00e0.js"
+  },
+  {
+    "revision": "b2207f053798c51305f7",
+    "url": "/js/chunk-vendors.b98c9e36.js"
+  },
+  {
+    "revision": "1ecee0c343b1bc0443f2e6eba14ed6ff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
