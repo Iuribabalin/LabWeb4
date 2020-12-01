348500b8c2d76009c1e1b6e5371a96cfc65e51cd
Iuribabalin <babalin2011@gmail.com>
2020-12-01 16:41:19 +0300

    new front

--- src/main/java/servlets/AuthServlet.java
+++ src/main/java/servlets/AuthServlet.java
@@ -10,15 +10,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/auth") //Логин на сайт
+@WebServlet("/login") //Логин на сайт
 public class AuthServlet extends HttpServlet{
     @EJB
     private Auth auth;
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/login").forward(req, resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
+        String username = req.getParameter("name");
+        String password = req.getParameter("pass");
         String result = auth.login(username, password);
         if (result.equals("No such user found") || result.equals("Not authorized")){
             resp.setStatus(403);
