dde53adfa8e5b4a56ec9e0382d476aa557e1d537
Iuribabalin <babalin2011@gmail.com>
2020-12-15 06:06:26 +0300

    Спокойной ночи, а вот нет пили лабу

---
+++ src/main/webapp/precache-manifest.47abc41caad696b7e80a7f1124a46284.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4ef03d2df7548679d3",
+    "url": "/css/app.94f61de7.css"
+  },
+  {
+    "revision": "a0858fc038701fb657a3b9953c61dae8",
+    "url": "/img/duck.a0858fc0.png"
+  },
+  {
+    "revision": "bd17b32b5cc018d0fdb46f3e482da28a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d4ef03d2df7548679d3",
+    "url": "/js/app.90ccbde9.js"
+  },
+  {
+    "revision": "92b4ce414a2aedceb142",
+    "url": "/js/chunk-1e47a9e8.c5c3f7e8.js"
+  },
+  {
+    "revision": "34b3ec8b4d8c8c916047",
+    "url": "/js/chunk-5136defe.c28c5044.js"
+  },
+  {
+    "revision": "e58d1f01855fa8eb9563",
+    "url": "/js/chunk-678b4bde.a76b14f7.js"
+  },
+  {
+    "revision": "ba70f4678ad943cbd09b",
+    "url": "/js/chunk-741f5406.6000c9d4.js"
+  },
+  {
+    "revision": "b2207f053798c51305f7",
+    "url": "/js/chunk-vendors.b98c9e36.js"
+  },
+  {
+    "revision": "1ecee0c343b1bc0443f2e6eba14ed6ff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
